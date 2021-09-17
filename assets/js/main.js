a1d8d1b1e803514e8eabb11f88e9e03a4242951e
Yamamoto-Kentaro <89883733+Yamamoto-Kentaro@users.noreply.github.com>
2021-09-17 10:11:22 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1: '琴',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '指',
           // q2: 'えええ',
           // q3: 'おおお'
         },
